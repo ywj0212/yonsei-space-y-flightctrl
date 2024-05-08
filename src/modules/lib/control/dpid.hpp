ae3598c4413293283907655cdfdc5a0b4bdb8085
유우진 <ywj02120212@gmail.com>
2024-05-09 02:24:51 +0900

    feat: Create PID, Dual Loop PID Controller

--- src/modules/lib/control/dpid.hpp
+++ src/modules/lib/control/dpid.hpp
@@ -4,19 +4,26 @@
 #include <type_traits>
 
 template <class T, typename std::enable_if<std::is_floating_point<T>::value, int>::type = 0>
-class PIDController {
+class DualPIDController {
 private:
   T Kp;
   T Ki;
   T Kd;
+  T Kp_rate;
+  T Ki_rate;
+  T Kd_rate;
   T P;
   T I = 0;
   T D;
+  T P_rate;
+  T I_rate = 0;
+  T D_rate;
   T last_error = 0;
+  T last_rate_error = 0;
   unsigned long last_time;
 
 public:
-  PIDController(T propotional, T integral, T derivative) : Kp(propotional), Ki(integral), Kd(derivative), (millis()) {}
+  DualPIDController(T propotional, T integral, T derivative) : Kp(propotional), Ki(integral), Kd(derivative), (millis()) {}
   void updateParameter(T propotional, T integral, T derivative) {
     Kp = propotional;
     Ki = integral;
@@ -28,12 +35,20 @@ public:
     last_time = current_time;
     
     T error = target - current;
-    P  = Kp * error;
-    I += Ki * error * delta_time;
+    P  = Kp + error;
+    I += Ki + error * delta_time;
     D  = Kd * (error - last_error) / delta_time;
-    T output = P + I + D;
+    T target_rate = P;
+
+    T rate_error = target_rate - current_rate;
+    P_rate  = Kp_rate * rate_error;
+    I_rate += Ki_rate * rate_error * delta_time;
+    D_rate  = Kd_rate * (rate_error - last_rate_error) / delta_time;
+
+    T output = P_rate + I_rate + D_rate + P + I;
 
     last_error = error;
+    last_rate_error = rate_error;
     return output;
   }
 };
